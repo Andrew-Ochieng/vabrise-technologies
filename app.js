df84994582c7703f61942e2e240908a4f70eb54e
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-10-01 11:57:46 +0300

    add menu toggle

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-// code here
+// initialize variables
+const menu = document.getElementById('menu')
+const hambuger = document.getElementById('hambuger')
+const times = document.getElementById('times')
 
 
+function showMenu() {
+    menu.classList.toggle("hidden")
+    hambuger.style.display = "none"
+    times.style.display = "flex"
+} 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function hideMenu() {
+    menu.classList.toggle("hidden") 
+    times.style.display = "none"
+    hambuger.classList = "flex"
+} 
 
 
 
